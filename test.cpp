37acf03b78ef104ef242fb21346f3cbfd6556424
Catupper <nekarugo628@gmail.com>
2017-06-25 14:44:49 +0900

    rm iranai file

--- test.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-  int n = 2;
-  int* p = &n;
-  printf("%p\n", &p);
-  printf("%p\n", &n);
-}
--- test.py
+++
@@ -1,8 +0,0 @@
-class test():
-    def __init__(self, x):
-        self.x = x
-
-    def setx(self, a):
-        self.x = a
-
-
